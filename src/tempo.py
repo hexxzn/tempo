d7d7f5c9fe9b760c42e442116bd2d99f1e05fce1
George <georgekilburg@gmail.com>
2021-11-19 18:56:31 -0500

    remove token

--- src/tempo.py
+++ src/tempo.py
@@ -13,4 +13,4 @@ async def on_ready():
     bot.load_extension('cogs.music')
     await bot.change_presence(activity=discord.Game(name="!help"))
 
-bot.run("ODk3ODY0ODg2MDk1MzQzNjg3.YWb31g.ylh37p49WhH5uyuy1AMcEcaFW-4")
+bot.run("")
