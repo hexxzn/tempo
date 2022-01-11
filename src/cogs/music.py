8f7303622b9baf828163298fbf030ae6475bdcd8
George <georgekilburg@gmail.com>
2022-01-11 05:25:21 -0500

    added !next command
    !next allows user to add song at queue position zero. song plays immediately after current song.

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -10,12 +10,12 @@ class Music(commands.Cog):
         self.bot.add_listener(self.bot.music.voice_update_handler, 'on_socket_response')
         self.bot.music.add_event_hook(self.track_hook)
 
-    @commands.command(name='play')   # play a song or add to queue (needs cleanup)
+    @commands.command(name='play')   # play a song or add to queue
     async def play(self, ctx, *, query):
-        print("!play")   # console print for debugging
+        print("!play")
         try:
-            member = ctx.author.name   # get name of user giving command
-            channel = ctx.author.voice.channel   # get voice channel of user giving command
+            member = ctx.author.name
+            channel = ctx.author.voice.channel
             player = self.bot.music.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))   # connect player to node
         except AttributeError:   # if user is not in accessible voice channel ('User' object has no attribute 'voice')
             return await ctx.send('Unable to locate user/voice channel.')
@@ -74,41 +74,95 @@ class Music(commands.Cog):
                 await ctx.send("**Warning**: Bot needs __Manage Messages__ permission to remove track selection messages.")
             await ctx.send('**Queued**: ' + track["info"]["title"])
 
-    @commands.command(name='skip')
+    @commands.command(name='next')   # play after current song (first in queue)
+    async def next(self, ctx, *, query):
+        print("!next")
+        try:
+            member = ctx.author.name
+            channel = ctx.author.voice.channel
+            player = self.bot.music.player_manager.create(ctx.guild.id, endpoint=str(ctx.guild.region))   # connect player to node
+        except AttributeError:   # if user is not in accessible voice channel ('User' object has no attribute 'voice')
+            return await ctx.send('Unable to locate user/voice channel.')
+        except Exception as error:   # except doesn't recognize NodeException?
+            print(error)
+            return await ctx.send('No available nodes.')
+
+        if not player.is_connected:
+            player.store('channel', ctx.channel.id)
+            await self.connect_to(ctx.guild.id, ctx.author.voice.channel.id)   # connect to voice channel
+
+        player = self.bot.music.player_manager.get(ctx.guild.id)
+        query = f'ytsearch:{query}'
+        results = await player.node.get_tracks(query)
+        tracks = results['tracks'][0:5]
+        query_result = '__**0.** None__ \n'
+        i = 0
+
+        for track in tracks:
+            i = i + 1
+            query_result = query_result + f'**{i}.** {track["info"]["title"]}\n'
+            # track_list = Embed(color=discord.Color.from_rgb(0, 198, 236))
+            track_list = Embed()
+            track_list.title = "__Enter Track Number__"
+            track_list.description = query_result
+        embed_message = await ctx.channel.send(embed=track_list)
+
+        def check(m):
+            return m.author.id == ctx.author.id
+
+        response = await self.bot.wait_for('message', check=check)
+        if not response.content.isnumeric() or int(response.content) not in range(1, len(tracks) + 1):
+            try:
+                await embed_message.delete()
+                await response.delete()
+            except:
+                await ctx.send("**Warning**: Bot needs __Manage Messages__ permission to remove track selection messages.")
+            return
+
+        track = tracks[int(response.content)-1]
+        player.add(requester=ctx.author.id, track=track, index=0)
+        if not player.is_playing:
+            try:
+                await embed_message.delete()
+                await response.delete()
+            except:
+                await ctx.send("**Warning**: Bot needs __Manage Messages__ permission to remove track selection messages.")
+            await ctx.send('**Now Playing**: ' + track["info"]["title"])
+            await player.play()
+            await player.set_volume(15)
+        else:
+            try:
+                await embed_message.delete()
+                await response.delete()
+            except:
+                await ctx.send("**Warning**: Bot needs __Manage Messages__ permission to remove track selection messages.")
+            await ctx.send('**Next**: ' + track["info"]["title"])
+
+    @commands.command(name='song')   # show current track info
+    async def song(self, ctx):
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            if player != None and player.current != None:
+                await ctx.send('**Now Playing**: ' + player.current.title)
+            else:
+                await ctx.send('Bot is not playing music.')
+        except Exception as error:
+            print(error)
+
+    @commands.command(name='skip')   # skip to next track in queue
     async def skip(self, ctx):
-        """!skip || skip to next song in queue"""
         print("!skip")
         try:
             player = self.bot.music.player_manager.get(ctx.guild.id)
             await player.skip()
-            if len(player.queue) > 0:
+            if len(player.queue) >= 0:
                 track = player.current.title
                 await ctx.send('**Now Playing**: ' + track)
         except Exception as error:
             print(error)
 
-    @commands.command(name='pause')
-    async def pause(self, ctx):
-        """!pause || pause playback"""
-        print("!pause")
-        try:
-            player = self.bot.music.player_manager.get(ctx.guild.id)
-            await player.set_pause(True)
-            await ctx.send('**Paused**: ' + "Use command **!resume** to unpause")
-        except Exception as error:
-            print(error)
-
-    @commands.command(name='resume')
-    async def resume(self, ctx):
-        """!resume || resume playback"""
-        print("!resume")
-        player = self.bot.music.player_manager.get(ctx.guild.id)
-        await player.set_pause(False)
-        await ctx.send('**Resumed**: ' + player.current.title)
-
-    @commands.command(name='stop')
+    @commands.command(name='stop')   # stop playback and clear queue
     async def stop(self, ctx):
-        """!stop || stop playback and clear queue"""
         print("!stop")
         try:
             player = self.bot.music.player_manager.get(ctx.guild.id)
@@ -126,21 +180,25 @@ class Music(commands.Cog):
             await ctx.send('Bot is not playing music.')
             print(error)
 
-    @commands.command(name='rw')
-    async def rw(self, ctx, seconds):
-        """!rw <seconds> || rewind given number of seconds"""
-        print("!rw")
+    @commands.command(name='pause')   # pause playback
+    async def pause(self, ctx):
+        print("!pause")
         try:
             player = self.bot.music.player_manager.get(ctx.guild.id)
-            await player.seek(player.position - int(seconds) * 1000)
-            await ctx.send("**Rewind**: " + seconds + " seconds \n" + "Buffering...")
+            await player.set_pause(True)
+            await ctx.send('**Paused**: ' + "Use command **!resume** to unpause")
         except Exception as error:
-            await ctx.send('Bot is not playing music.')
             print(error)
 
-    @commands.command(name='ff')
+    @commands.command(name='resume')   # unpause playback
+    async def resume(self, ctx):
+        print("!resume")
+        player = self.bot.music.player_manager.get(ctx.guild.id)
+        await player.set_pause(False)
+        await ctx.send('**Resumed**: ' + player.current.title)
+
+    @commands.command(name='ff')   # fast forward given number of seconds
     async def ff(self, ctx, seconds):
-        """!ff <seconds> || fast forward given number of seconds"""
         print("!ff")
         try:
             player = self.bot.music.player_manager.get(ctx.guild.id)
@@ -150,9 +208,19 @@ class Music(commands.Cog):
             await ctx.send('Bot is not playing music.')
             print(error)
 
-    @commands.command(name='restart')
+    @commands.command(name='rw')   # rewind given number of seconds
+    async def rw(self, ctx, seconds):
+        print("!rw")
+        try:
+            player = self.bot.music.player_manager.get(ctx.guild.id)
+            await player.seek(player.position - int(seconds) * 1000)
+            await ctx.send("**Rewind**: " + seconds + " seconds \n" + "Buffering...")
+        except Exception as error:
+            await ctx.send('Bot is not playing music.')
+            print(error)
+
+    @commands.command(name='restart')   # return to beginning of current track
     async def restart(self, ctx):
-        """!restart || return to beginning of current track"""
         print("!restart")
         try:
             player = self.bot.music.player_manager.get(ctx.guild.id)
@@ -162,9 +230,8 @@ class Music(commands.Cog):
             await ctx.send('Bot is not playing music.')
             print(error)
 
-    @commands.command(name='qq')
+    @commands.command(name='qq')   # show length of queue
     async def qq(self, ctx):
-        """!qq || check length of queue"""
         print("!qq")
         player = self.bot.music.player_manager.get(ctx.guild.id)
         if player == None:
@@ -172,44 +239,35 @@ class Music(commands.Cog):
         else:
             await ctx.send("**Queue**: " + str(len(player.queue)))
 
-    @commands.command(name='help')
+    @commands.command(name='help')   # show command list in text channel
     async def help(self, ctx):
         print("!help")
         help_menu = Embed()
         help_menu.title = "__Tempo Commands__"
         help_menu.description = ("**!play <song name, artist>** \n" +
                                 "— play a song or add to queue \n" +
-                                "**!stop** \n" +
-                                "— stop playback and clear queue \n" +
+                                "**!next <song name, artist>** \n" +
+                                "— play after current song (first in queue) \n" +
                                 "**!song** \n" +
-                                "— show name of current track \n" +
+                                "— show current track info \n" +
                                 "**!skip** \n" +
                                 "— skip to next track in queue \n" +
+                                "**!stop** \n" +
+                                "— stop playback and clear queue \n" +
                                 "**!pause** \n" +
                                 "— pause playback \n" +
                                 "**!resume** \n" +
                                 "— unpause playback \n" +
-                                "**!restart** \n" +
-                                "— return to beginning of current track \n" +
                                 "**!ff <seconds>** \n" +
                                 "— fast forward given number of seconds \n" +
                                 "**!rw <seconds>** \n" +
                                 "— rewind given number of seconds \n" +
+                                "**!restart** \n" +
+                                "— return to beginning of current track \n" +
                                 "**!qq** \n" +
-                                "— check length of queue \n" +
-                                "__**Developed by Hexxzn**__")
+                                "— show length of queue \n" +
+                                "\n __**Developed by Hexxzn**__")
         await ctx.channel.send(embed=help_menu)
-
-    @commands.command(name='song')
-    async def song(self, ctx):
-        try:
-            player = self.bot.music.player_manager.get(ctx.guild.id)
-            if player != None and player.current != None:
-                await ctx.send('**Now Playing**: ' + player.current.title)
-            else:
-                await ctx.send('Bot is not playing music.')
-        except Exception as error:
-            print(error)
 
     async def track_hook(self, event):
         if isinstance(event, lavalink.events.QueueEndEvent):
@@ -230,22 +288,21 @@ class Music(commands.Cog):
     
     # @commands.command(name='clean')
     # async def clean(self, ctx):
-    #     """!clean || delete messages in text channel"""
     #     try:
     #         print("!clean")
     #         await ctx.channel.purge()
     #     except Exception as error:
     #         print(error)
 
+    # @commands.command(name='test')
+    # async def tempot(self, ctx):
+    #     player = self.bot.music.player_manager.get(ctx.guild.id)
+    #     print(player.queue)
+
     # @commands.command(name='tempo')
     # async def tempo(self, ctx):
     #     """!tempo || bot developed by hexxzn"""
     #     await ctx.send(':wave:')
-
-    # @commands.command(name='test')
-    # async def tempot(self, ctx):
-    #     player = self.bot.music.player_manager.get(ctx.guild.id)
-    #     await player.queue.clear()
 
 def setup(bot):
     bot.add_cog(Music(bot))
