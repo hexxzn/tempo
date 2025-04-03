bc953584aa656edf136e1085d4a1442d6eab473a
George <georgekilburg@gmail.com>
2025-04-03 18:31:55 -0400

    5.5.4
    Updated bash scripts (one private)

--- src/restart.sh
+++ src/restart.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-kill $(ps aux | grep '[p]ython3 main.py' | awk '{print $2}')
-kill $(ps aux | grep '[j]ava -jar lavalink.jar' | awk '{print $2}')
+echo "Stopping Lavalink and Tempo..."
+pkill -f lavalink.jar && echo "Lavalink stopped..." || echo "Lavalink not running..."
+pkill -f main.py && echo "Tempo stopped..." || echo "Tempo not running..."
+sleep 3
 
-echo "Killing Processes..."
-
+# Specify output/log file name
 _now=$(date +"%m_%d_%Y")
 _file="$_now.out"
 
-cd
-cd git
-cd tempo
-nohup java -jar lavalink.jar &> "$_file" &
-
+# Run Lavalink and create log file
+cd ~/git/tempo
 echo "Starting Lavalink..."
+nohup java -jar lavalink.jar &> "$_file" &
+sleep 3
 
+# Run Tempo and create log file
 cd src
-nohup python3 main.py &> "$_file" &
-
 echo "Starting Tempo..."
-
-echo "Please Wait..."
+nohup python3 main.py &> "$_file" &
+sleep 3
